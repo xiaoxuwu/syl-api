dd1c53313801ee78b52ee2076286f246416ecc96
kluangkote <katiel97@hotmail.com>
2019-05-03 08:51:21 -0700

    Add models

--- internal/admin.py
+++ internal/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Link
+from .models import Preferences, Link
 
+admin.site.register(Preferences)
 admin.site.register(Link)
--- internal/models.py
+++ internal/models.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
 
 # automatically generate token for every new user
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
@@ -10,8 +11,31 @@ def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
         Token.objects.create(user=instance)
 
+class Preferences(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # image = models.ImageField()
+
+# create Preferences object when User is created
+@receiver(post_save, sender=User)
+def create_user_preferences(sender, instance, created, **kwargs):
+    if created:
+        Preferences.objects.create(user=instance)
+
+# update preferences when User is updated
+@receiver(post_save, sender=User)
+def save_user_preferences(sender, instance, **kwargs):
+    instance.preferences.save()
+
 class Link(models.Model):
+    creator = models.ForeignKey(Preferences, on_delete=models.CASCADE, null=True)
     url = models.URLField()
 
     def __str__(self):
         return self.url
+
+class Event(models.Model):
+    link = models.ForeignKey(Link, on_delete=models.CASCADE)
+    time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.time
