5abee398989d5ef33000b5c2cb32ce94998aa132
Katrina Wijaya <katrina@thewijayas.com>
2019-05-11 22:45:19 -0700

    Fixed permissions indentation

--- internal/permissions.py
+++ internal/permissions.py
@@ -35,32 +35,32 @@ class HasEventPermission(permissions.BasePermission):
     link events. Create access is currently public.
     """
     def has_permission(self, request, view):
-      """
-      Applies global permissions to incoming requests. The API prevents any
-      PUT/PATCH/DELETE functionality. POST is a public route.
-      Admins: can GET all events
-      Link Owners: can GET events for links they own
-      Public: cannot GET any events 
-      TODO: set up synchronous API key in place of public POST access
-      """
-      if request.user.is_superuser or request.method == 'POST':
-        return True
+        """
+        Applies global permissions to incoming requests. The API prevents any
+        PUT/PATCH/DELETE functionality. POST is a public route.
+        Admins: can GET all events
+        Link Owners: can GET events for links they own
+        Public: cannot GET any events 
+        TODO: set up synchronous API key in place of public POST access
+        """
+        if request.user.is_superuser or request.method == 'POST':
+            return True
 
-      link_id = request.query_params.get('link', None)
-      username = request.query_params.get('username', None)
-      if link_id is not None:
-        try:
-          link = Link.objects.get(pk=link_id)
-        except:
-          link = None
-        if link is not None and request.user == link.creator:
-          return True
-      elif username is not None:
-        try:
-          user = User.objects.get(username=username)
-        except:
-          user = None
-        if user is not None and request.user == user:
-          return True
+        link_id = request.query_params.get('link', None)
+        username = request.query_params.get('username', None)
+        if link_id is not None:
+            try:
+                link = Link.objects.get(pk=link_id)
+            except:
+                link = None
+            if link is not None and request.user == link.creator:
+                return True
+        elif username is not None:
+            try:
+                user = User.objects.get(username=username)
+            except:
+                user = None
+            if user is not None and request.user == user:
+                return True
 
-      return False
+        return False
