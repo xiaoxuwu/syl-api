6f434406cf4f6577d264daaf300ce8b47047d28f
Katrina Wijaya <katrina@thewijayas.com>
2019-05-11 22:07:09 -0700

    Fixed indentation width to 4 spaces

--- internal/views.py
+++ internal/views.py
@@ -51,200 +51,184 @@ class EventViewSet(viewsets.ModelViewSet):
     permission_classes = (AllowAny, HasEventPermission)
     
     def parse_date(self, date):
-      try:
-        if date is not None:
-          return parser.parse(date)
-      except:
-        return None
+        try:
+            if date is not None:
+                return parser.parse(date)
+        except:
+            return None
 
     def parse_int(self, num):
-      try:
-        if num is not None:
-          return int(num)
-      except:
-        return None
+        try:
+            if num is not None:
+                return int(num)
+        except:
+            return None
 
     def filter_by_date_range(self, queryset):
-      """
-      Given optional start/end parameters, returns events in the specified 
-      custom time period.
-      """
-      start = self.request.query_params.get('start', None)
-      start = self.parse_date(start)
-      end = self.request.query_params.get('end', None)
-      end = self.parse_date(end).replace(hour=23, minute=59, second=59)
+        """
+        Given optional start/end parameters, returns events in the specified 
+        custom time period.
+        """
+        start = self.request.query_params.get('start', None)
+        start = self.parse_date(start)
+        end = self.request.query_params.get('end', None)
+        end = self.parse_date(end).replace(hour=23, minute=59, second=59)
 
-      if start is not None or end is not None:
-        if start is not None and end is not None:
-          queryset = queryset.filter(time__range=[start, end])
-        elif start:
-          queryset = queryset.filter(time__gte=start)
-        else:
-          queryset = queryset.filter(time__lte=end)
-      return queryset
+        if start is not None or end is not None:
+            if start is not None and end is not None:
+                queryset = queryset.filter(time__range=[start, end])
+            elif start:
+                queryset = queryset.filter(time__gte=start)
+            else:
+                queryset = queryset.filter(time__lte=end)
+        return queryset
 
     def filter_by_month(self, queryset):
-      """
-      Given optional month/year parameters, returns events in the specified
-      month/year. Year defaults to current year.
-      """
-      month_str = self.request.query_params.get('month', None)
-      month_int = self.parse_int(month_str)
-      year_str = self.request.query_params.get('year', None)
-      year_int = self.parse_int(year_str)
-      curr_year_str = dateformat.format(timezone.now(), 'Y')
-      curr_year_int = self.parse_int(curr_year_str)
+        """
+        Given optional month/year parameters, returns events in the specified
+        month/year. Year defaults to current year.
+        """
+        month_str = self.request.query_params.get('month', None)
+        month_int = self.parse_int(month_str)
+        year_str = self.request.query_params.get('year', None)
+        year_int = self.parse_int(year_str)
+        curr_year_str = dateformat.format(timezone.now(), 'Y')
+        curr_year_int = self.parse_int(curr_year_str)
 
-      if month_str is not None and 1 <= month_int <= 12:
-        if year_str is not None and 1999 <= year_int <= curr_year_int:
-          return queryset.filter(time__year=year_str, time__month=month_str)
-        elif curr_year_str is not None:
-          return queryset.filter(time__year=curr_year_str, time__month=month_str)
-      return self.filter_by_date_range(queryset)
+        if month_str is not None and 1 <= month_int <= 12:
+            if year_str is not None and 1999 <= year_int <= curr_year_int:
+                return queryset.filter(time__year=year_str, time__month=month_str)
+            elif curr_year_str is not None:
+                return queryset.filter(time__year=curr_year_str, time__month=month_str)
+        return self.filter_by_date_range(queryset)
 
     def filter_by_time(self, queryset):
-      """
-      Given an optional time parameter, return events in the specified time
-      period. Offers week and 7days option to facilitate frontend visualization.
-      """
-      today = timezone.now()
-      date = today.date()
-      month = dateformat.format(today, 'm')
-      year = dateformat.format(today, 'Y')
-      last_monday = today.date() - timedelta(days=today.weekday())
-      this_monday = today.date() + timedelta(days=-today.weekday(), weeks=1)
-      last_7_days = today.date() - timedelta(days=7)
-      last_30_days = today.date() - timedelta(days=30)
-      last_90_days = today.date() - timedelta(days=90)
+        """
+        Given an optional time parameter, return events in the specified time
+        period. Offers week and 7days option to facilitate frontend visualization.
+        """
+        today = timezone.now()
+        date = today.date()
+        month = dateformat.format(today, 'm')
+        year = dateformat.format(today, 'Y')
+        last_monday = today.date() - timedelta(days=today.weekday())
+        this_monday = today.date() + timedelta(days=-today.weekday(), weeks=1)
+        last_7_days = today.date() - timedelta(days=7)
+        last_30_days = today.date() - timedelta(days=30)
+        last_90_days = today.date() - timedelta(days=90)
 
-      time = self.request.query_params.get('time', None)
-      if time is not None:
-        time = time.lower()
-        return {
-          'latest': queryset.order_by('-time')[:1],
-          'today': queryset.filter(time__date=date),
-          'week': queryset.filter(time__range=[last_monday, this_monday]),
-          'month': queryset.filter(time__year=year, time__month=month),
-          '7days': queryset.filter(time__range=[last_7_days, today]),
-          '30days': queryset.filter(time__range=[last_30_days, today]),
-          '90days': queryset.filter(time__range=[last_90_days, today]),
-        }.get(time, self.filter_by_month(queryset))
-      return self.filter_by_month(queryset)
+        time = self.request.query_params.get('time', None)
+        if time is not None:
+            time = time.lower()
+            return {
+                'latest': queryset.order_by('-time')[:1],
+                'today': queryset.filter(time__date=date),
+                'week': queryset.filter(time__range=[last_monday, this_monday]),
+                'month': queryset.filter(time__year=year, time__month=month),
+                '7days': queryset.filter(time__range=[last_7_days, today]),
+                '30days': queryset.filter(time__range=[last_30_days, today]),
+                '90days': queryset.filter(time__range=[last_90_days, today]),
+            }.get(time, self.filter_by_month(queryset))
+        return self.filter_by_month(queryset)
 
     def filter_by_id(self):
-      """
-      Given an optional link or username parameter, return events for the
-      specified link(s).
-      TODO: if no link_id/user is found, default filter with username=request.username?
-      """
-      queryset = Event.objects.all()
-      link_id = self.request.query_params.get('link', None)
-      username = self.request.query_params.get('username', None)
-      if link_id is not None:
-        queryset = queryset.filter(link=link_id)
-      elif username is not None:
-        queryset = queryset.filter(link__creator__username=username)
-      return queryset
+        """
+        Given an optional link or username parameter, return events for the
+        specified link(s).
+        TODO: if no link_id/user is found, default filter with username=request.username?
+        """
+        queryset = Event.objects.all()
+        link_id = self.request.query_params.get('link', None)
+        username = self.request.query_params.get('username', None)
+        if link_id is not None:
+            queryset = queryset.filter(link=link_id)
+        elif username is not None:
+            queryset = queryset.filter(link__creator__username=username)
+        return queryset
 
     def get_queryset(self):
-      """
-      Given optional filtering parameters, return any corresponding events.
-      """
-      queryset = self.filter_by_id()
-      queryset = self.filter_by_time(queryset)
-      return queryset
+        """
+        Given optional filtering parameters, return any corresponding events.
+        """
+        queryset = self.filter_by_id()
+        queryset = self.filter_by_time(queryset)
+        return queryset
 
     @action(detail=False, methods=['get'], url_path='stats', name='Event Stats')
     def get_event_stats(self, request):
-      """
-      Given a method parameter, returns event data in the specified format. Can
-      return 'count' statistics or the original Event data.
-      TODO: figure out return format
-      TODO: restrict date range for daily/weekly/monthly/yearly data?
-      TODO: stats by device type, geographic region, time of day/week
-      """
-      output = []
-      queryset = self.filter_by_id()
-      time = self.request.query_params.get('time', None)
-      method = self.request.query_params.get('method', None)
-      if time is None:
-        queryset = self.filter_by_month(queryset)
-        if method is not None and method.lower() == 'count':
-          return Response({ 'count': queryset.count() })
-        serializer = self.serializer_class(queryset, many=True)
-        output = serializer.data
-      else:
+        """
+        Given a method parameter, returns event data in the specified format. Can
+        return 'count' statistics or the original Event data.
+        TODO: figure out return format according to recharts docs
+        TODO: restrict date range for daily/weekly/monthly/yearly data
+        TODO: stats by device type, geographic region, time of day/week
+        """
+        output = []
+        queryset = self.filter_by_id()
+        time = self.request.query_params.get('time', None)
+        method = self.request.query_params.get('method', None)
+        if time is None:
+            queryset = self.filter_by_month(queryset)
+            if method is not None and method.lower() == 'count':
+                return Response({ 'count': queryset.count() })
+            serializer = self.serializer_class(queryset, many=True)
+            return Response(serializer.data)
+
         time = time.lower()
-        if time in ['daily', 'weekly', 'monthly', 'yearly']:
-          queryset = {
+        if time not in ['daily', 'weekly', 'monthly', 'yearly']:
+            queryset = self.filter_by_time(queryset)
+            if method is not None and method.lower() == 'count':
+                return Response({ 'count': queryset.count() })
+            serializer = self.serializer_class(queryset, many=True)
+            return Response(serializer.data)
+
+        queryset = {
             'daily': queryset.annotate(period=TruncDay('time')),
             'weekly': queryset.annotate(period=TruncWeek('time')),
             'monthly': queryset.annotate(period=TruncMonth('time')),
             'yearly': queryset.annotate(period=TruncYear('time')),
-          }.get(time).order_by('period')
+        }.get(time).order_by('period')
 
-          # TODO: there is probably a better way to do this query
-          # Do we even want this query
-          # Leaving these options here for reviewers to weigh in
-
-          # Creates Event model from data cols
-          # queryset = queryset \
-          #           .values('period') \
-          #           .annotate(ids=ArrayAgg('id'), links=ArrayAgg('link'), times=ArrayAgg('time')) \
-          #           .values('period', 'ids', 'links', 'times')
-          # for q in queryset:
-          #   data = list(EventSerializer(Event(id=id, link_id=link, time=time), many=False).data \
-          #     for id, link, time in zip(q['ids'], q['links'], q['times']))
-          #   output.append({ 'period': q['period'], 'count': len(data), 'events': data})
-
-          # Fetches Event entry by primary key
-          queryset = queryset \
+        # Fetches Event entry by primary key
+        output = []
+        queryset = queryset \
                     .values('period') \
                     .annotate(event_ids=ArrayAgg('id')) \
                     .values('period', 'event_ids')
-          for q in queryset:
+        for q in queryset:
             data = list(EventSerializer(Event.objects.get(pk=id), many=False).data for id in q['event_ids'])
-            output.append({ 'period': q['period'], 'count': len(data), 'events': data })
-
-          # Just returns array of timestamps per period
-          # output = queryset \
-          #         .values('period') \
-          #         .annotate(times=ArrayAgg('time')) \
-          #         .values('period', 'times')
-        else:
-          queryset = self.filter_by_time(queryset)
-          if method is not None and method.lower() == 'count':
-            return Response({ 'count': queryset.count() })
-          serializer = self.serializer_class(queryset, many=True)
-          output = serializer.data
-
-      return Response(output)
+            output.append({ 
+                'period': q['period'], 
+                'count': len(data), 
+                'events': data 
+            })
+        return Response(output)
 
     def create(self, request):
-      """
-      Creates an event for the specified link_id. Admins may specify a time
-      parameter.
-      """
-      link_id = self.request.POST.get('link', None)
-      time = self.request.POST.get('time', None)
-      time = self.parse_date(time)
+        """
+        Creates an event for the specified link_id. Admins may specify a time
+        parameter.
+        """
+        link_id = self.request.POST.get('link', None)
+        time = self.request.POST.get('time', None)
+        time = self.parse_date(time)
 
-      if link_id is not None:
-        event = Event.objects.create(link_id=link_id)
-        if request.user.is_superuser and time is not None:
-          event = Event.objects.create(link_id=link_id, time=time)
-        event.save()
-        serializer = EventSerializer(event)
-        return Response(serializer.data)
-      return Response(status=status.HTTP_400_BAD_REQUEST)
+        if link_id is not None:
+            event = Event.objects.create(link_id=link_id)
+            if request.user.is_superuser and time is not None:
+                event = Event.objects.create(link_id=link_id, time=time)
+            event.save()
+            serializer = EventSerializer(event)
+            return Response(serializer.data)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
 
     # Disable PUT/DELETE endpoints.
     def update(self, request, pk=None):
-      return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
     def partial_update(self, request, pk=None):
-      return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
     def destroy(self, request, pk=None):
-      return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
 class PreferenceViewSet(mixins.ListModelMixin,
                         mixins.UpdateModelMixin,
