d2aeacd57f1a1300fd789267f65f5b5c0a80f901
kluangkote <katiel97@hotmail.com>
2019-05-12 19:14:10 -0700

    Add text and image field to Link model

--- internal/models.py
+++ internal/models.py
@@ -40,6 +40,7 @@ class Link(models.Model):
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
     url = models.URLField()
     text = models.CharField(max_length=200, blank=True, default=None)
+    image = models.ImageField(blank=True, default=None, null=True)
 
     def __str__(self):
         return self.url
